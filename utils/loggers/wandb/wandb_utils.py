8c8fc8a6552f15cea7c3ecc5539e22fab9d17b8a
CodeWracker <dev.rodrigofs@gmail.com>
2024-04-01 00:37:12 -0300

    removed all path management from files. path shall be managed outside of this repo or the user

--- utils/loggers/wandb/wandb_utils.py
+++ utils/loggers/wandb/wandb_utils.py
@@ -13,8 +13,6 @@ from utils.general import LOGGER, colorstr
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[3]  # YOLOv5 root directory
-if str(ROOT) not in sys.path:
-    sys.path.append(str(ROOT))  # add ROOT to PATH
 RANK = int(os.getenv("RANK", -1))
 DEPRECATION_WARNING = (
     f"{colorstr('wandb')}: WARNING ⚠️ wandb is deprecated and will be removed in a future release. "
